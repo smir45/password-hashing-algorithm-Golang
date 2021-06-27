0180c7ce776b1ee359c5f0bdd13cb248f6ec1540
Samir Mishra <smir.mishra1551@gmail.com>
2021-06-28 01:58:45 +0545

    creating a password hashing algorithm

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func main() {
+	var password string
+	fmt.Println("Your Password should not contain any spaces")
+	fmt.Print("Please enter your password: ")
+	fmt.Scanln(&password)
+	hash, _ := HashPassword(password)
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hashed Password:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+}
+
+// Decryption of hashed password algorithm is comming soon....
